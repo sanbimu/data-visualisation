7b713676d2451a858f98aff7a2d66faf1f09d23e
sanbimu <119463187+sanbimu@users.noreply.github.com>
2023-01-24 15:29:21 +0100

    second graph

--- backup.js
+++ backup.js
@@ -17,12 +17,11 @@ const canvasOne = '<canvas id="canvasOne" width="800" height="500" class="graph"
 table1.insertAdjacentHTML ('beforebegin', canvasOne);
 
 const canvasTwo = '<canvas id="canvasTwo" width="800" height="500" class="graph"></canvas>';
-table2.insertAdjacentHTML ('beforebegin', canvasTwo);
-
+table2.insertAdjacentHTML ('beforebegin', canvasTwo );
 
 ////////////// CHART 1:
 
-const dataCells = document.querySelectorAll("td"); // Select the table cells containing the data
+var dataCells = document.querySelectorAll("td"); // Select the table cells containing the data
 
 var data = {};                                  // create a variable and assign an empty object to it
 var currentCountry;                             // create undefined variable
@@ -71,37 +70,85 @@ var myChart = new Chart(ctx, {
 
 ////////////// CHART 2: 
 
-var countries = [...table2.querySelectorAll("td")].filter(td => td.cellIndex === 1).map(td => td.innerText);
-var thirdColumn = [...table2.querySelectorAll("td")].filter(td => td.cellIndex === 2).map(td => td.innerText);
-var fourthColumn = [...table2.querySelectorAll("td")].filter(td => td.cellIndex === 3).map(td => td.innerText);
-var years = [...table2.querySelectorAll("thead th")].map(th => th.innerText)
+var dataCells = document.querySelectorAll("td");                      // Select the table cells containing the data
+
+var data = {};                                                        // create a variable and assign an empty object to it
+var currentCountry;                                                   // create undefined variable
+for (var i = 0; i < dataCells.length; i++) {                          // create for loop to iterate over array of dataCells
+  if (!isNaN(parseFloat(dataCells[i].textContent))){                  //  if the data inside the cell is a number
+    data[currentCountry].push(parseFloat(dataCells[i].textContent));  //  push it into the array for the current country in the data object
+}
+  else {                                                              // else, if it's not a number
+    currentCountry = dataCells[i].textContent;                        // set the value of currentCountry to the value of the current cell
+    data[currentCountry] = [];                                        // create an empty array for the current country in the data object
+  }};
+
+
+var datasets = []                               
+var colors = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#17becf", "#9edae5", "#fd8d3c", "#b3de69", "#fdb462", "#bc80bd", "#ccebc5", "#ffed6f", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#c7c7c7", "#bcbd22", "#dbdb8d", "#9edae5", "#fd8d3c", "#b3de69", "#fdb462", "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f", "#ffa07a", "#20b2aa", "#228b22", "#f0fff0", "#90ee90", "#ffb6c1", "#ffc0cb", "#e6e6fa"];
+var colorIndex = 0;                             
+for (var country in data) {                     
+    datasets.push({   
+        label: country,                         
+        data: data[country],                   
+        backgroundColor: colors[colorIndex]    
+    });
+    colorIndex++;                               
+}
 
 var ctx = document.getElementById("canvasTwo").getContext("2d");
+  datasets.sort(function(a, b) {
+  return b.data[0] - a.data[0];
+  });
 var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-      labels: countries,
-      datasets: [{
-          label: years[2],
-          data: thirdColumn,
-          backgroundColor: "rgba(173, 128, 255, 0.5)",
-          borderColor: "rgba(173, 128, 255, 0.5)",
-          borderWidth: 1
-      },
-      {
-          label: years[3],
-          data: fourthColumn,
-          backgroundColor: "rgba(75, 192, 192, 0.5)",
-          borderColor: "rgba(75, 192, 192, 0.5)",
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          y: {
-              beginAtZero: true
-          }
-      }
-  }
+    type: "bar",
+    data: {
+        labels: ["2007-09", "2010-12"], // x-axis labels
+        datasets: datasets
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
 
+
+////////////// CHART 3:
+
+var data = {
+  labels: [],
+  datasets: [{
+      label: "Live chart",
+      data: [],
+      backgroundColor: "rgba(173, 128, 255, 0.5)",
+      borderColor: "rgba(173, 128, 255, 0.5)",
+      borderWidth: 1
+  }]
+};
+
+var ctx = document.getElementById("canvasZero").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: data,
+  options: {}
+});
+
+setInterval(() => {
+  let labels = []
+  let axeY = []
+  fetch('https://canvasjs.com/services/data/datapoints.php')
+    .then(response => response.json())
+    .then(res => {
+      res.forEach(el => {
+          labels.push(el[0])
+          axeY.push(el[1])
+      })
+        myChart.data.labels = labels
+        myChart.data.datasets[0].data =axeY;
+        myChart.update();
+    })
+    .catch(error => console.error(error));
+}, 1000);
